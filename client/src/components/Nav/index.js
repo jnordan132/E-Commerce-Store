566ea14cde773cdc446d3cc419abebccc50876b2
jacob nordan <jacob.nordan113322@gmail.com>
2022-01-14 17:23:28 -0800

    changing some slight styling in sizing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,15 +7,15 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
+        <ul className="flex-row" style={{ listStyle: "none"}}>
           <li className="mx-1">
-            <Link to="/orderHistory">
+            <Link to="/orderHistory" style={{ textDecoration: 'none' }}>
               Order History
             </Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
+            <a href="/" onClick={() => Auth.logout()}  style={{ textDecoration: 'none' }}>
               Logout
             </a>
           </li>
@@ -23,14 +23,14 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="flex-row" style={{ listStyle: "none", float: "right" }}>
+        <ul className="flex-row" style={{ listStyle: "none" }}>
           <li className="mx-1">
-            <Link to="/signup">
+            <Link to="/signup"  style={{ textDecoration: 'none' }}>
               Signup
             </Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
+            <Link to="/login"  style={{ textDecoration: 'none' }}>
               Login
             </Link>
           </li>
@@ -42,7 +42,7 @@ function Nav() {
   return (
     <header className="flex-row px-1">
       <h1>
-        <Link style={{ textDecoration: 'none'}} to="/">
+        <Link style={{ textDecoration: 'none' }}>
           Shoe Fanatic
         </Link>
       </h1>
