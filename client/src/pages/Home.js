8ac3bae372cc08adb62670f427bd3f1664d7a189
Jay <jaypushcode@gmail.com>
2022-01-17 23:46:03 -0500

    Changed image crop to shoes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,8 +6,8 @@ import CategoryMenu from "../components/CategoryMenu";
 const Home = () => {
   return (
     <div className="container">
-      <div className="headerImgDiv" style={{ width: '100vw', height: '80vh', objectFit:'contain'}}>
-        <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 50%"}} ></img>
+      <div className="headerImgDiv" style={{ width: '100vw', height: '60vh', objectFit:'contain'}}>
+        <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 90%"}} ></img>
       </div>
       <CategoryMenu />
       <ProductList />
