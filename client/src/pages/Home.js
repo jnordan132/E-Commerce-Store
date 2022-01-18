78915d9cf37cdd6bfe2b5ba8f2212998c44dc8ae
Jay <jaypushcode@gmail.com>
2022-01-18 00:15:12 -0500

    Adding Contrast to MJ picture

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="container">
       <div className="headerImgDiv" style={{ width: '100vw', height: '60vh', objectFit:'contain'}}>
-        <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 90%"}} ></img>
+        <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 90%", filter:"contrast(1.09)"}} ></img>
       </div>
       <CategoryMenu />
       <ProductList />
