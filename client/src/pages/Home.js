98fb332001799c06c56a11c1da4c15b87dba85c6
Jay <jaypushcode@gmail.com>
2022-01-18 00:31:12 -0500

    Adding sudo h1 tag over MJ picture (tbc)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
   return (
     <div className="container">
       <div className="headerImgDiv" style={{ width: '100vw', height: '60vh', objectFit:'contain'}}>
+      {/* <h1 style={{position:'relative',zIndex:'1',fontSize:'180px',fontFamily:'sans-serif'}}> SHOE FANATIC </h1> */}
         <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 90%", filter:"contrast(1.09)"}} ></img>
       </div>
       <CategoryMenu />
