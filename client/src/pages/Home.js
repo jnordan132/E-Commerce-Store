90202d51cc137f88f11baa43815ad876d1f3ff4f
Jay <jaypushcode@gmail.com>
2022-01-18 01:13:19 -0500

    Experimental Words on Header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div className="container">
       <div className="headerImgDiv" style={{ width: '100vw', height: '60vh', objectFit:'contain'}}>
-      {/* <h1 style={{position:'relative',zIndex:'1',fontSize:'180px',fontFamily:'sans-serif'}}> SHOE FANATIC </h1> */}
-        <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 90%", filter:"contrast(1.09)"}} ></img>
+      <h1 style={{position:'absolute',top:'35%',left:'50%',transform:'translate(-50%,-50%)',zIndex:'1',fontSize:'180px',fontFamily:'Montserrat',fontWeight:'550',whiteSpace:"nowrap",color:"white",textShadow:'0 1px 1px rgb(0 0 0 / 15%), 0 2px 2px rgb(0 0 0 / 15%), 0 4px 4px rgb(0 0 0 / 15%), 0 6px 8px rgb(0 0 0 / 15%), 0 8px 16px rgb(0 0 0 / 15%)'}}> SHOE FANATIC </h1>
+        <img src="../images/MJ-header.jpg" style={{width:'inherit',height:'inherit',objectFit:'cover', objectPosition:"50% 90%", filter:"contrast(1.09)",filter:"blur(1px)",transform:'scale(1.01)'}} ></img>
       </div>
       <CategoryMenu />
       <ProductList />
