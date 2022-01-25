ef18c0929c5deebbfe7ca1c08ee0ece5a37c859a
jacob nordan <jacob.nordan113322@gmail.com>
2022-01-25 15:24:52 -0800

    set up one link in data.json, fixed target='_blank' with search results

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -48,7 +48,7 @@ function SearchBar({ placeholder, data }) {
         <div className="dataResult">
           {filteredData.slice(0, 15).map((value, key) => {
             return (
-              <a className="dataItem" href={value.link} target="_blank">
+              <a className="dataItem" href={value.link}>
                 <p>{value.title} </p>
               </a>
             );
