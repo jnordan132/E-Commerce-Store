5f35a5f4319c77b88b04e1cb6f831d8848ea74df
Jacob Nordan <jacob.nordan113322@gmail.com>
2023-03-08 09:47:50 -0500

    footer update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (
-        <div style={{ backgroundImage: 'linear-gradient(315deg, #a71d31 0%, #3f0d12 74%)', width: '100%', height: '12vh', marginTop: '3%', textAlign: 'center' }}>
-            <br/>
-            <h3 style={{ color: 'white', fontWeight: 'bold' }}>Shoe Fanatic</h3>
-            <span style={{ color: 'white' }}>© 2022 Shoe Fanatic</span>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        backgroundImage: "linear-gradient(315deg, #a71d31 0%, #3f0d12 74%)",
+        width: "100%",
+        height: "12vh",
+        marginTop: "3%",
+        textAlign: "center",
+      }}
+    >
+      <br />
+      <h3 style={{ color: "white", fontWeight: "bold" }}>Shoe Fanatic</h3>
+      <span style={{ color: "white" }}>© 2023 Shoe Fanatic</span>
+    </div>
+  );
 }
 
 export default Footer;
